ead2bda587b6845e6146cc3afbd09a5924028288
Reut Raman <soolamis@vivaldi.net>
2020-04-20 19:29:25 +0300

    init db

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const User = dbConnect.import('./models/user');
 const Message = dbConnect.import('./models/message');
 User.sync();
 Message.sync();
-User.findOrCreate({ user: 'init'});
-Message.findOrCreate({ author: 1, text: 'test'});
+User.findOrCreate({ where: { username: 'System' }});
+Message.findOrCreate({ where: { author: 1, text: 'Hi all!!!'}});
 
 app.use(express.static('./build'));
 
