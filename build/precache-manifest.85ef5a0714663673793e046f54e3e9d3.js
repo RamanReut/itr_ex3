bc4eb94c5d052a54abb1eca2cc23ecd96b3e299b
Reut Raman <soolamis@vivaldi.net>
2020-04-20 20:06:08 +0300

    socket fix

---
+++ build/precache-manifest.85ef5a0714663673793e046f54e3e9d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53289447098e207bdb0750efa1dfa77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00c586fda686fb37e18",
+    "url": "/static/css/2.1c085d96.chunk.css"
+  },
+  {
+    "revision": "06d0316a0f669d15d1d5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f00c586fda686fb37e18",
+    "url": "/static/js/2.37433949.chunk.js"
+  },
+  {
+    "revision": "bbcbbb56570ef421a517be0ed83cf07b",
+    "url": "/static/js/2.37433949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d0316a0f669d15d1d5",
+    "url": "/static/js/main.b940f9f9.chunk.js"
+  },
+  {
+    "revision": "2b56d173f87c1f04e364",
+    "url": "/static/js/runtime-main.00217ea4.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39ca4390dd618e5c209fe7083f8ca411.js"
+  "/precache-manifest.85ef5a0714663673793e046f54e3e9d3.js"
 );
 
 self.addEventListener('message', (event) => {
